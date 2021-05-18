3b40d45c04f61b5c6de13e817b9df373ded0fb36
Daniel Clements <daniel.clements14@gmail.com>
2021-05-18 02:04:36 +0100

    started property listing models

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class propertyListing(models.Model):
+
+    # property_header = models.CharField(max_length=254)
+    # bedrooms = models.IntegerField(max_digits=2, default=0)
+    # bathrooms = models.IntegerField(max_digits=2, default=0)
+
+    
+
+    def __str__(self):
+        return self.property_header
+
+class property_type(models.Model):
+
+    type = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.type
