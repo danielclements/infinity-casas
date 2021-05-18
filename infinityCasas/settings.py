bc1d3f2ca25bcd631d8f7676fa8730808467f4ec
Daniel Clements <daniel.clements14@gmail.com>
2021-05-18 02:03:19 +0100

    updated settings with base template directory

--- infinityCasas/settings.py
+++ infinityCasas/settings.py
@@ -58,12 +58,18 @@ ROOT_URLCONF = 'infinityCasas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
+                # Required by Allauth
                 'django.template.context_processors.request',
+
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
