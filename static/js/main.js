95119920a3e5948fa2ebe55eab916ada3f7cdd9a
danielclements <daniel.clements14@gmail.com>
2022-04-04 18:36:56 +0200

    added mobile navbar, only redered on screens beloew 768px

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.hamburger').click(function(){
         $(this).toggleClass("is-active");
+        $(".mobile-nav").toggleClass("mobile-nav-active");
     });
 });
