3f0bb92d0d090c0be206f7f23d5df66c9c26bf11
danielclements <daniel.clements14@gmail.com>
2022-04-08 18:49:13 +0200

    added basic searhc function to all-properties view, will expand in future

--- properies/views.py
+++ properies/views.py
@@ -1,5 +1,14 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import property
 
 # Create your views here.
 def allProperties(request):
-    return render(request, 'properies/properties.html') 
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    properties = property.objects.filter(title__icontains=q)
+    context = {'properties': properties}
+ 
+    return render(request, 'properies/properties.html', context) 
+
+ 
