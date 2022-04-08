d54bc1396f66eca965ed4489d1ebc4607d419c33
danielclements <daniel.clements14@gmail.com>
2022-04-08 19:16:53 +0200

    added small properties found counter below search bar, displays hpw many properties found after search query

--- properies/views.py
+++ properies/views.py
@@ -1,13 +1,23 @@
 from multiprocessing import context
 from django.shortcuts import render
+from django.db.models import Q
 from .models import property
 
 # Create your views here.
 def allProperties(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
-    properties = property.objects.filter(title__icontains=q)
-    context = {'properties': properties}
+    properties = property.objects.filter(
+        Q(title__icontains=q) or
+        Q(town__icontains=q)
+        )
+    property_count = properties.count()
+    
+    context = {'properties': properties,
+                'property_count': property_count
+                }
+
+
  
     return render(request, 'properies/properties.html', context) 
 
