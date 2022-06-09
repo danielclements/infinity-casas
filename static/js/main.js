167f4146525840222da04ef0661d199e891d502b
danielclements <daniel.clements14@gmail.com>
2022-06-09 15:34:00 +0200

    added begining of property search / filter button

--- static/js/main.js
+++ static/js/main.js
@@ -4,3 +4,10 @@ $(document).ready(function() {
         $(".mobile-nav").toggleClass("mobile-nav-active");
     });
 });
+
+$(document).ready(function(){
+    $(".property-search-filter").click(function(){
+      $(".search-filters").toggleClass("search-filter-open-js");
+      $(".property-search-filter").toggleClass("property-search-filter-open-js").toggleClass("property-search-container-active-color-toogle");
+    });
+});
