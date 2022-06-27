82d387cbf8b31ba42bad6f103e88defa8c0b7612
danielclements <daniel.clements14@gmail.com>
2022-06-27 17:28:07 +0200

    xml trouble shotting

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
+urlpatterns = [
+    path('xml',views.propertyXMLViewSet.as_view(), name="property-xml"),
+]
+
 # urlpatterns = [
-#     path('xml',views.propertyXMLViewSet.as_view()),
+#     path('xml',views.allPropertiesxml, name='allPropertiesxml'),
 # ]
 
-urlpatterns = [
-    path('xml',views.allPropertiesxml, name='allPropertiesxml'),
-]
--- api/views.py
+++ api/views.py
@@ -1,50 +1,45 @@
-# from rest_framework_xml.renderers import XMLRenderer
+from rest_framework_xml.renderers import XMLRenderer
 from django.shortcuts import render, HttpResponse
 from django.urls import path
 from requests import Response
 from properies.models import property, property_image, currency_type, price_freq, property_type, provence, country, agent, property_status
-# from rest_framework import routers, serializers, viewsets
-# from rest_framework_xml.parsers import XMLParser
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
+from rest_framework import routers, serializers, viewsets
+from rest_framework_xml.parsers import XMLParser
+from rest_framework_xml.renderers import XMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 
-# class propertySerializer(serializers.ModelSerializer):
-#     provence = serializers.CharField(source='provence.provence', read_only=True)
+class propertySerializer(serializers.ModelSerializer):
+    provence = serializers.CharField(source='provence.provence', read_only=True)
     
 
-#     class Meta:
-#         model = property
-#         fields = ('title', 'town', 'provence', 'desc', 'ref')
+    class Meta:
+        model = property
+        fields = ('title', 'town', 'provence', 'desc', 'ref')
 
-# class propertyXMLRenderer(XMLRenderer):
+class propertyXMLRenderer(XMLRenderer):
 
-#     item_tag_name = 'property'
-#     creation_tag_name = 'kyero'
-#     kyeroFeedVersion = 'feed_version'
+    root_tag_name = 'feed'
+    item_tag_name = 'property'
+    creation_tag_name = 'Kyero'
 
-#     def _to_xml(self, xml, data):
-
-#         xml.startElement(self.creation_tag_name, {})
-#         xml.endElement(self.creation_tag_name)
-        
-#         super()._to_xml(xml, data)
-
-# class propertyXMLViewSet(APIView):
-#     renderer_classes = [propertyXMLRenderer, ]
-
-#     def get(self, request, format=None):
-#         queryset = property.objects.all()
-#         serializer_class = propertySerializer(queryset, many=True)
-#         return Response(serializer_class.data)
+    def _to_xml(self, xml, data):
+        xml.startElement(self.creation_tag_name, {})
+        xml.characters('3')
+        self._to_xml(xml, data)
+        xml.endElement(self.creation_tag_name)
         
 
+class propertyXMLViewSet(APIView):
+    renderer_classes = [propertyXMLRenderer, ]
 
-def allPropertiesxml(request):
-    
-    properties = property.objects.all()
-    context = {'properties': properties,
-                }
+    def get(self, request, format=None):
+        queryset = property.objects.all()
+        serializer_class = propertySerializer(queryset, many=True)
+        return Response(serializer_class.data)
 
-    return HttpResponse(open('api/templates/api/property.xml').read(), context, content_type='text/xml')
+
+
+
